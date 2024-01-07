6cdac51392616d5f3f057bc42493cc195ff811b9
lebantion <58497274+lebantion@users.noreply.github.com>
2024-01-07 13:46:22 -0500

    initial for Android

---
+++ Android/app/src/main/java/com/example/cardier/ui/login/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.cardier.ui.login
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+import com.example.cardier.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login);
+    }
+
+}
