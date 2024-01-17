9183a390dc11f8ddbf3efeadf01cd0877abc3774
John Choi <johnchoi1003@icloud.com>
2024-01-17 02:47:47 -0500

    Card prototype implemented

---
+++ IOS/Cardier/dtos/BusinessCardPayload.swift
@@ -0,0 +1,20 @@
+//
+//  BusinessCardPayload.swift
+//  Cardier
+//
+//  Created by John Choi on 1/17/24.
+//
+
+import Foundation
+
+struct BusinessCardPayload {
+    var displayName: String
+
+    var email: String
+
+    var phone: String
+
+    var companyName: String
+
+    var companyLogo: String?
+}
---
+++ IOS/Cardier/views/components/BusinessCard.swift
@@ -0,0 +1,58 @@
+//
+//  BusinessCard.swift
+//  Cardier
+//
+//  Created by John Choi on 1/17/24.
+//
+
+import SwiftUI
+
+struct BusinessCard: View {
+
+    private let screenWidth = UIScreen.main.bounds.width
+
+    let cardPayload: BusinessCardPayload
+
+    private let rotationValue: Double = 270
+
+    var body: some View {
+        VStack {
+            VStack {
+                HStack {
+                    // Render optional image
+                    Image(systemName: "photo")
+                        .rotationEffect(.degrees(rotationValue))
+                    Spacer()
+                    EmptyView()
+                }
+
+                Spacer()
+
+                HStack {
+                    EmptyView()
+
+                    Spacer()
+
+                    VStack(alignment: .leading) {
+                        Text(cardPayload.displayName)
+                        Text(cardPayload.phone)
+                        Text(cardPayload.email)
+                        Text(cardPayload.companyName)
+                    }
+                    .rotationEffect(.degrees(rotationValue))
+                    .frame(minHeight: 200)
+                }
+            }
+
+            .padding()
+            .frame(width: 300, height: 400)
+            .cornerRadius(15)
+            .background(Color(uiColor: .green))
+            .shadow(color: Color.gray.opacity(0.5), radius: 5, x: 0, y: 5)
+        }
+    }
+}
+
+#Preview {
+    BusinessCard(cardPayload: BusinessCardPayload(displayName: "John Doe", email: "john.doe@email.com", phone: "999-999-9999", companyName: "My Company"))
+}
