c6e2178233951632d039c92e9e4b7c512d8e6cfc
Sonsungwo <son.253@buckeyemail.osu.edu>
2024-01-20 14:48:59 -0500

    frist push

---
+++ backend/src/main/java/osu/example/cardier/CardierApplication.java
@@ -0,0 +1,13 @@
+package osu.example.cardier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardierApplication.class, args);
+	}
+
+}
