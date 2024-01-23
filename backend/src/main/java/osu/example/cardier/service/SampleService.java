8b1ff86fdb5a7331e29a6f74aa85da83183b978e
Sonsungwo <son.253@buckeyemail.osu.edu>
2024-01-23 00:27:43 -0500

    add endpoint

--- backend/src/main/java/osu/example/cardier/service/SampleService.java
+++ backend/src/main/java/osu/example/cardier/service/SampleService.java
@@ -1,4 +1,19 @@
 package osu.example.cardier.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
 public class SampleService {
+
+    @GetMapping(value = "/status/ping" , produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getFact () throws JsonProcessingException{
+
+        return "pong";
+    }
 }
