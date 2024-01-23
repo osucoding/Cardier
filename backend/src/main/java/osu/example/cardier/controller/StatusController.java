6571f4af960cdf4e7b779e74fc9b383eebadc340
Sonsungwo <son.253@buckeyemail.osu.edu>
2024-01-23 00:54:27 -0500

    fix endpoint

--- backend/src/main/java/osu/example/cardier/controller/StatusController.java
+++ backend/src/main/java/osu/example/cardier/controller/StatusController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/status")
 public class StatusController {
 
-    @GetMapping(value = "/status/ping", produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(value = "/ping", produces = MediaType.TEXT_PLAIN_VALUE)
     public String ping() {
         return "pong";
     }
